9c8edea10961cd27e393d94929fd570c5452035f
Don Park <donp@donp.org>
2020-01-27 09:45:25 -0800

    check for inplay

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,7 @@
 use std::fs;
 
 use redis::{Commands, RedisError};
-use serde::{Serialize, Deserialize};
+use serde::{Deserialize, Serialize};
 
 mod order;
 use crate::order::Order;
@@ -13,24 +13,29 @@ fn main() {
 }
 
 fn app(config: Config) -> Result<u32, RedisError> {
-    let mut pubclient = rdsetup(&config.redis)?;
-    let mut ps = rdsub(&mut pubclient);
-
-    let msg = ps.get_message()?;
-    let arb_id: String = msg.get_payload()?;
-    println!("redis {}", arb_id);
-
     let mut client = rdsetup(&config.redis)?;
-    let hkey = [String::from("arb:"), arb_id].concat();
-    let json: String = client.hget(&hkey, "json")?;
-    let order: Order = serde_yaml::from_str(&json).unwrap();
-    println!("{} {:#?}", hkey, order);
+    let inplay_exists = client.exists("inplay")?;
+    if (inplay_exists) {
+        let inplay: String = client.get("inplay")?;
+    } else {
+        let mut pubclient = rdsetup(&config.redis)?;
+        let mut ps = rdsub(&mut pubclient);
+
+        let msg = ps.get_message()?;
+        let arb_id: String = msg.get_payload()?;
+        println!("new Order {}", arb_id);
+
+        let hkey = [String::from("arb:"), arb_id].concat();
+        let json: String = client.hget(&hkey, "json")?;
+        let order: Order = serde_yaml::from_str(&json).unwrap();
+        println!("{} {:#?}", hkey, order);
+    }
     Ok(0)
 }
 
 #[derive(Debug, Serialize, Deserialize)]
 struct Config {
-    redis: String
+    redis: String,
 }
 
 fn configload() -> Config {
