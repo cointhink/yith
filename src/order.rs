a10983d42d99a30a03280c22c7c027c9180d2723
Don Park <donp@donp.org>
2020-01-27 13:00:40 -0800

    Vec<Force>, Luke.

--- src/order.rs
+++ src/order.rs
@@ -4,10 +4,36 @@ use serde::{Deserialize, Serialize};
 pub struct Order {
     id: String,
     pair: Pair,
+    cost: f64,
+    profit: f64,
+    ask_books: Books,
+    bid_books: Books,
 }
 
 #[derive(Debug, Serialize, Deserialize)]
 pub struct Pair {
-    base: String,
-    quote: String,
+    Field0: String, //base
+    Field1: String, //quote
+}
+
+#[derive(Debug, Serialize, Deserialize)]
+pub struct Books {
+    askbid: String,
+    books: Vec<Book>,
+}
+
+#[derive(Debug, Serialize, Deserialize)]
+pub struct Book {
+    market: Market,
+}
+
+#[derive(Debug, Serialize, Deserialize)]
+pub struct Market {
+    source: Source,
+}
+
+#[derive(Debug, Serialize, Deserialize)]
+pub struct Source {
+    name: String,
+    url: String,
 }
