fdbd90332abc11c98f75a750b8d42218154472eb
Don Park <donp@donp.org>
2020-02-06 10:56:34 -0800

    test_pubkey_to_addr

--- src/exchanges/ddex3.rs
+++ src/exchanges/ddex3.rs
@@ -1,8 +1,7 @@
 use crate::config;
 use crate::types;
-use hex::decode;
 use reqwest::header;
-use secp256k1::{Message, PublicKey, Secp256k1, SecretKey, Signing};
+use secp256k1::{Message, PublicKey, Secp256k1, SecretKey};
 use serde::{Deserialize, Serialize};
 use std::time::Duration;
 use std::time::{SystemTime, UNIX_EPOCH};
@@ -118,21 +117,11 @@ fn build_token(token: &mut String, privkey: &str, msg: &str) {
     let secret_key = SecretKey::from_slice(privbytes).expect("32 bytes, within curve order");
     let public_key = PublicKey::from_secret_key(&secp, &secret_key);
     let pubkey_bytes = &public_key.serialize_uncompressed();
-    let mut output = [0u8; 32];
-    let mut hasher = Keccak::v256();
-    hasher.update(&pubkey_bytes[1..]);
-    hasher.finalize(&mut output);
-    let addr = &output[12..]; //.slice(-20)
-    let hash_full = format!("\u{0019}Ethereum Signed Message:\n{}{}", msg.len(), msg);
+    println!("pubkey: {:?}", hex::encode(&pubkey_bytes[..]));
+    let addr = pubkey_to_addr(pubkey_bytes);
     let mut msg_hash = [0u8; 32];
-    let mut hasher2 = Keccak::v256();
-    hasher2.update(&pubkey_bytes[1..]);
-    hasher2.finalize(&mut msg_hash);
-    let secp2 = Secp256k1::new();
-    let scmsg = Message::from_slice(&msg_hash).unwrap();
-    let sig = secp2.sign(&scmsg, &secret_key);
-    //let (recovery_id, serialize_sig) = sig.serialize_compact();
-    let sig_bytes = sig.serialize_compact();
+    hash_msg(&mut msg_hash, msg, pubkey_bytes);
+    let sig_bytes = sign_bytes(&msg_hash, secret_key);
     println!("sig: {:?}", hex::encode(&sig_bytes[..]));
     let signed_hash = msg_hash;
     token.push_str(
@@ -144,6 +133,31 @@ fn build_token(token: &mut String, privkey: &str, msg: &str) {
         )
         .as_str(),
     );
+}
+
+pub fn pubkey_to_addr(pubkey_bytes: &[u8; 65]) -> [u8; 20] {
+    let mut output = [0u8; 32];
+    let mut hasher = Keccak::v256();
+    hasher.update(&pubkey_bytes[1..]);
+    hasher.finalize(&mut output);
+    let mut sized_output = [0u8;20];
+    sized_output.copy_from_slice(&output[12..32]);
+    sized_output
+}
+
+pub fn hash_msg(mut msg_hash: &mut [u8], msg: &str, pubkey_bytes: &[u8; 65]) {
+    let hash_full = format!("\u{0019}Ethereum Signed Message:\n{}{}", msg.len(), msg);
+    let mut hasher = Keccak::v256();
+    hasher.update(&pubkey_bytes[1..]);
+    hasher.finalize(&mut msg_hash);
+}
+
+pub fn sign_bytes(msg_hash: &[u8], secret_key: SecretKey) -> [u8;64] {
+    let secp2 = Secp256k1::new();
+    let scmsg = Message::from_slice(&msg_hash).unwrap();
+    let sig = secp2.sign(&scmsg, &secret_key);
+    //let (recovery_id, serialize_sig) = sig.serialize_compact();
+    sig.serialize_compact()
 }
 
 pub fn order(os: OrderSheet) {
@@ -159,10 +173,22 @@ pub fn make_market_id(swapped: bool, base: &types::Ticker, quote: &types::Ticker
 
 #[cfg(test)]
 mod tests {
-    use super::build_token;
+    use super::*;
 
     #[test]
-    fn test_bad_add() {
+    fn test_pubkey_to_addr() {
+        let pubkey = "041ea3510efdb57c6cf0dc77a454b4f5b95775f9606b0f7d7a294b47aae57b21882e6c4888d050992b58a0640066ab72adff7575c07d201716c40b9146624eedb4";
+        let pubkey_bytes = hex::decode(pubkey).unwrap();
+        let mut pubkey_sized_bytes = [0u8; 65];
+        pubkey_sized_bytes.copy_from_slice(&pubkey_bytes);
+        let addr_bytes = pubkey_to_addr(&pubkey_sized_bytes);
+        let addr = hex::encode(addr_bytes);
+        let good_addr = "ed6d484f5c289ec8c6b6f934ef6419230169f534";
+        assert_eq!(addr, good_addr);
+    }
+
+    #[test]
+    fn test_build_token() {
         let mut token = String::from("");
         let privkey = "e4abcbf75d38cf61c4fde0ade1148f90376616f5233b7c1fef2a78c5992a9a50";
         let msg = "HYDRO-AUTHENTICATION@1566380397473";
