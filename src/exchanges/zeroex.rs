d31a187856d1e306697605348e7fcc8ccdc15846
Don P <donp@donp.org>
2020-02-10 15:09:19 -0800

    radar build bits

--- src/exchanges/zeroex.rs
+++ src/exchanges/zeroex.rs
@@ -1,13 +1,28 @@
 use crate::config;
 use crate::types;
 use serde::{Deserialize, Serialize};
+use std::time::{SystemTime, UNIX_EPOCH};
 
 #[derive(Debug, Serialize, Deserialize)]
 pub struct OrderSheet {
-    r#type: String,
-    quantity: f64,
-    price: f64,
-    expiration: f64,
+    r#type: BuySell,
+    quantity: String,
+    price: String,
+    expiration: String,
+}
+
+#[derive(Debug, Serialize, Deserialize)]
+pub struct BuildResponse {
+    status: i64,
+    error: String,
+}
+
+#[derive(Debug, Serialize, Deserialize)]
+pub enum BuySell {
+    #[serde(rename = "BUY")]
+    Buy,
+    #[serde(rename = "SELL")]
+    Sell,
 }
 
 pub fn build(
@@ -25,28 +40,40 @@ pub fn build(
     let mut market_id = make_market_id(market.swapped, &market.base, &market.quote);
     let mut qty = offer.base_qty;
     let mut price = offer.quote;
+    let mut ab = askbid;
+    let askbid_other = askbid.otherside();
     if market.swapped {
-        let ab = askbid.otherside();
+        ab = &askbid_other;
         let (s_qty, s_price) = offer.swap();
         println!(
             "unswapped {:#?} {} {}-{} {}@{}",
             ab, market.source.name, market.quote, market.base, s_qty, s_price
         );
     }
+    let side = match ab {
+        types::AskBid::Ask => BuySell::Buy,
+        types::AskBid::Bid => BuySell::Sell,
+    };
+    let expire_time =  SystemTime::now()
+            .duration_since(UNIX_EPOCH)
+            .unwrap()
+            .as_millis();
     let sheet = OrderSheet {
-        r#type: "UserOrderType".to_string(),
-        quantity: qty,
-        price: price,
-        expiration: 500.0,
+        r#type: side,
+        quantity: format!("{}", qty),
+        price: format!("{}", price),
+        expiration: format!("{}", expire_time),
     };
     let mut url = exchange.build_url.clone();
-    url.push_str(format!("/markets/{}/order/limit", market_id).as_str());
+    url.push_str(format!("/{}/order/limit", market_id).as_str());
     println!("0x order {}", url);
     println!("{:#?}", sheet);
     let client = reqwest::blocking::Client::new();
-    let resp = client.post(exchange.build_url.as_str()).json(&sheet);
-    //.send()?;
-    //let body = resp.json::<HashMap<String, String>>()?;
+    let resp = client.post(url.as_str()).json(&sheet).send()?;
+    println!("{:#?} {}", resp.status(), resp.url());
+//    let body = resp.json::<BuildResponse>().unwrap();
+//    println!("{:#?}", body);
+    println!("{:#?}", resp.text()?);
     Ok(())
 }
 
