a047cbc7a5d4f4fc738a907465228bdba2af00c2
Don P <donp@donp.org>
2020-03-18 09:30:01 -0700

    lettre_email

--- src/email.rs
+++ src/email.rs
@@ -1,18 +1,16 @@
 use lettre::{EmailAddress, Envelope, SendableEmail, SmtpClient, Transport};
+use lettre_email::Email;
 
 pub fn send(to_addr: &str, subject: &str, body: &str) {
     let payload = format!("Subject: {}\n\n{}", subject, body);
-    let email = SendableEmail::new(
-        Envelope::new(
-            Some(EmailAddress::new("yith@donp.org".to_string()).unwrap()),
-            vec![EmailAddress::new(to_addr.to_string()).unwrap()],
-        )
-        .unwrap(),
-        format!("msgid-acb123"),
-        payload.into_bytes(),
-    );
+    let email = Email::builder() // to be used
+        .to(to_addr)
+        .from("yith@donp.org")
+        .subject(subject)
+        .body(body)
+        .build().unwrap();
     let mut mailer = SmtpClient::new_unencrypted_localhost().unwrap().transport();
-    let result = mailer.send(email);
+    let result = mailer.send(email.into());
     let word = if result.is_ok() { "sent" } else { "FAILED" };
     println!("email {} {} {}", to_addr, subject, word);
     println!("{}", body);
