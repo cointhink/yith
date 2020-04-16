38d9b26f4f2a29ac7a67f4bcc0cf7d7791d38954
Don P <donp@donp.org>
2020-04-15 17:42:49 -0700

    test case capture

--- src/exchanges/idex.rs
+++ src/exchanges/idex.rs
@@ -3,12 +3,12 @@ use crate::eth;
 use crate::exchange;
 use crate::types;
 use reqwest::header;
+use rlp;
 use secp256k1::SecretKey;
 use serde::{Deserialize, Serialize};
 use std::collections::HashMap;
 use std::fs;
 use std::time::Duration;
-use rlp;
 
 #[derive(Debug, Serialize, Deserialize)]
 pub enum BuySell {
@@ -151,7 +151,7 @@ impl exchange::Api for Idex {
             amount_sell: quote_qty.to_str_radix(10),
             address: format!("0x{}", address),
             nonce: nonce_response.nonce.to_string(),
-            expires: 0,
+            expires: 10000,
         }))
     }
 
@@ -165,7 +165,6 @@ impl exchange::Api for Idex {
             let privbytes = &hex::decode(privkey).unwrap();
             let secret_key = SecretKey::from_slice(privbytes).unwrap();
             let order_hash_bytes = order_params_hash(&order_sheet, exchange);
-            println!("{:?}", order_hash_bytes);
             let order_hash = eth::ethsign_hash_msg(&order_hash_bytes[..].to_vec());
             let (v, r, s) = eth::sign_bytes_vrs(&order_hash, &secret_key);
             let signed = OrderSheetSigned {
@@ -211,31 +210,40 @@ impl exchange::Api for Idex {
 pub fn order_params_hash(order: &OrderSheet, exchange: &config::ExchangeSettings) -> [u8; 32] {
     let expires = order.expires.to_string();
     let mut parts: Vec<Vec<u8>> = vec![
-        encode_str(&exchange.contract_address),
-        encode_str(&order.token_buy),
+        encode_addr(&exchange.contract_address),
+        encode_addr(&order.token_buy),
         encode_uint256(&order.amount_buy),
-        encode_str(&order.token_sell),
+        encode_addr(&order.token_sell),
         encode_uint256(&order.amount_sell),
         encode_uint256(&expires),
         encode_uint256(&order.nonce),
-        encode_str(&order.address),
+        encode_addr(&order.address),
     ];
-    let hashes = parts.iter_mut().fold(Vec::<u8>::new(), |mut memo, part| {
+    let hash_hex = parts.iter_mut().fold(Vec::<u8>::new(), |mut memo, part| {
         memo.append(part);
         memo
     });
+    let hashes = hex::decode(&hash_hex).unwrap();
+    println!(
+        "params len {} {}. decoded len {}",
+        hash_hex.len(),
+        std::str::from_utf8(&hash_hex).unwrap(),
+        hashes.len()
+    );
     eth::hash_msg(&hashes)
 }
 
-pub fn encode_str(str: &str) -> Vec<u8> { // 160bits/20bytes
-    hex::decode(str[2..].to_string()).unwrap()
+pub fn encode_addr(str: &str) -> Vec<u8> {
+    // 160bits/20bytes
+    str[2..].as_bytes().to_vec()
 }
 
-pub fn encode_uint256(numstr: &str) -> Vec<u8> { // 256bits/32bytes
+pub fn encode_uint256(numstr: &str) -> Vec<u8> {
+    // 256bits/32bytes
     let num = numstr.parse::<u128>().unwrap();
-    rlp_encode_int(num)
+    let rlp_bytes = rlp_encode_int(num);
+    hex::encode(rlp_bytes).as_bytes().to_vec()
 }
-
 
 pub fn rlp_encode_int(num: u128) -> Vec<u8> {
     let num_bytes = rlp::encode(&(num as u64));
@@ -245,34 +253,37 @@ pub fn rlp_encode_int(num: u128) -> Vec<u8> {
 pub fn left_pad_null(bytes: Vec<u8>, width: u8) -> Vec<u8> {
     let mut padded = Vec::<u8>::new();
     let bytes_len = bytes.len();
-    let left = (width as usize)- bytes_len;
-    for x in 0..left { padded.push(0) };
+    let left = (width as usize) - bytes_len;
+    for x in 0..left {
+        padded.push(0)
+    }
     padded.append(&mut bytes.clone());
     padded
 }
-
-
-/*
-    "tokenBuy": "0xd6e8a328c5c9b6cc4c917a50ecbe0aeb663c666e",
-    "amountBuy": "1000000000000000000",
-    "tokenSell": "0x0000000000000000000000000000000000000000",
-    "amountSell": "20354156573527349",
-    "address": "0x2dbdcec64db33e673140fbd0ceef610a273b84db",
-    "nonce": "1544",
-    "expires": 100000,
-    "v": 28,
-    "r": "0xc6ddcbdf69d0e20fe879d2405b40ee417773c8a177a5d7f4461f2310565ac3d1",
-    "s": "0x497cdfedfde3308bb9d9e80ea2eabff43c7a15fef0eb164c265e3855a1bd9073"
-*/
 
 #[cfg(test)]
 mod tests {
     use super::*;
 
+    static PRIVKEY_DDEX3: &str = "e4abcbf75d38cf61c4fde0ade1148f90376616f5233b7c1fef2a78c5992a9a50";
+
     #[test]
-    fn test_build_token() {
-        let token = build_token(PRIVKEY, MSG_V3);
-        let good_token = format!("0x{}#{}#0x{}", GOOD_ADDR, MSG_V3, GOOD_SIG_V3);
+    fn test_order_params() {
+        let address = eth::privkey_to_addr(PRIVKEY_DDEX3);
+        println!("{} {} ", PRIVKEY_DDEX3, address);
+
+/*
+  "tokenBuy": "0x0000000000000000000000000000000000000000",
+  "amountBuy": "150000000000000000",
+  "tokenSell": "0xcdcfc0f66c522fd086a1b725ea3c0eeb9f9e8814",
+  "amountSell": "1000000000000000000000",
+  "address": "0xed6d484f5c289ec8c6b6f934ef6419230169f534",
+  "nonce": 123,
+  "expires": 100000,
+*/
+        let good_r = "0x860874c6d650c646389e3a7fbcd835665e546cbafa9831438d3a71535c19c50f";
+        let good_s = "0x18205ecf4a6927e8653828c5508c3676f634c74051d9ef4f9216dbef43594a25";
+
         //assert_eq!(token, good_token);
     }
 }
